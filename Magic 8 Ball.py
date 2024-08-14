cb8df8bc4ce8d5e4bd0ed01ec779c854aeeb1396
ZahraAzimi43 <149286378+ZahraAzimi43@users.noreply.github.com>
2024-08-14 17:42:01 +0330

    Add files via upload

---
+++ Magic 8 Ball.py
@@ -0,0 +1,30 @@
+print("MAGIC 8 BALL\n\n")
+print("*HOW IT WORKS?*?")
+print("Think about a topic in your mind and then enter the number you want")
+print("Then the bot will say its opinion about the topic you thought about\n")
+print("*ATTENTION* ")
+print("1_You only have 6 chances!")
+print("2_Please don't enter an invalid number")
+print("3_Please don't enter a duplicate number\n")
+a="It is certain."
+b="Without a doubt."
+c="Don't do it!"
+d="It is decidedly so."
+e="Don't count on it."
+f="You may rely on it."
+g="outlook not so good."
+h="My sources say no!"
+input("1)  Enter a number from 1 to 10:\n")
+print(a,"\n")
+input("2)  Enter a number from 1 to 10:\n")
+print(b,"\n")
+input("3)  Enter a number from 1 to 10:\n")
+print(c,"\n")
+input("4)  Enter a number from 1 to 10:\n")
+print(d,"\n")
+input("5)  Enter a number from 1 to 10:\n")
+print(e,"\n")
+input("6)  Enter a number from 1 to 10:\n")
+print(f,"\n")
+print("Your chances are over!")
+print("GOOD LUCK")
---
+++ magic 8 ball 2.py
@@ -0,0 +1,46 @@
+print("MAGIC 8 BALL\n\n")
+print("*HOW IT WORKS?*?")
+print("Think about a topic in your mind and then enter the number you want")
+print("Then the bot will say its opinion about the topic you thought about\n")
+print("*ATTENTION* ")
+print("1_You only have 1 chance!")
+print("2_Please don't enter an invalid number\n")
+choice = input("Enter a number:\n")
+if choice == "0":
+ print("It is certain.\n")
+if choice == "1":
+ print("It is decidedly so.\n")
+if choice == "2":
+ print("Without a doubt.\n")
+if choice == "3":
+ print("Yes - definitely.\n")
+if choice == "4":
+ print("You may rely on it.\n")
+if choice == "5":
+ print("As I see it, yes.\n")
+if choice == "6":
+ print("Most likely.\n")
+if choice == "7":
+ print("Outlook good.\n")
+if choice == "8":
+ print("Yes.\n")
+if choice == "9":
+ print("Signs point to yes.\n")
+if choice == "10":
+ print("Try again.\n")
+if choice == "11":
+ print("Very doubtful.\n")
+if choice == "12":
+ print("Better not tell you now.\n")
+if choice == "13":
+ print("Cannot predict now.\n")
+if choice == "14":
+ print("Concentrate and ask again.\n")
+if choice == "15":
+ print("Don't count on it.\n")
+if choice == "16":
+ print("My reply is no.\n")
+if choice == "17":
+ print("My sources say no.\n")
+if choice == "18":
+ print("Outlook not so good.\n")
